f970d0c820a44048a24ebe39dddfc9610d545c62
kociumbas <kociumbas@outlook.com>
2016-02-16 00:24:12 +0100

    CSResztaZDzielenia

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_6WynikDzieleniaReszta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 37;
+            int b = 11;
+            int wynik = 0;
+            Console.WriteLine("Program oblicza resztę dzielenia dla liczb 37 i 11");
+            wynik = a % b; 
+            Console.WriteLine("Reszta z dzielenia liczb wynosi: {0}", wynik);
+            Console.ReadLine();
+        }
+    }
+}
